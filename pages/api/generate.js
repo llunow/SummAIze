12333c94cb3edce888daa78ba095afc720767cc5
llunow <llunow@gmail.com>
2023-10-09 19:50:54 +0200

    Updated command in generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -86,7 +86,7 @@ async function generateChatPrompt(url) {
     return {
         prompt: [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Can you help create a 250 word summary of the following article?"},
+            {"role": "user", "content": "Summarize this article in 250 words."},
             {"role": "user", "content": "The article is formatted as markdown."},
             {"role": "user", "content": `The title of the article is ${article.title}.`},
             {"role": "user", "content": `The article is as follows: \n${truncatedString}`}
