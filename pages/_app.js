eb06395bc4300519c13075df207d35117f027587
llunow <llunow@gmail.com>
2023-11-06 15:53:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,6 @@ function MyApp({Component, pageProps}) {
         <>
             <Component {...pageProps} />
             <Analytics/>
-            <Script strategy="afterInteractive" src="https://analytics.umami.is/script.js"
-                    data-website-id="6fa86d7f-8c94-4fcb-94c4-5e9a08f7e2f2" />
         </>
     );
 }
